3285f899f05f95cdaa13a285329f6d65295272b1
Antelop1204 <lilinyang06@163.com>
2017-06-28 13:33:59 +0800

    update

--- monSlaveRepl.py
+++ monSlaveRepl.py
@@ -5,21 +5,21 @@
 
 import os
 import MySQLdb
-import requests
 import sys
 import os.path
 import urllib
 import urllib2
 import logging
 import smtplib
+import requests
 from email.mime.text import MIMEText
 
 #mysql configure
 MYSQL_SETTINGS = {
-    "host": "xx",
-    "port": xx,
+    "host": "XXX",
+    "port": XXX,
     "user": "dba",
-    "passwd": "xx"
+    "passwd": "XXX"
 }
 #log configure
 logging.basicConfig(
@@ -31,11 +31,11 @@ logging.basicConfig(
 )
 #smtp configure
 
-MAIL_REVIEW_SMTP_SERVER = 'xxx'
+MAIL_REVIEW_SMTP_SERVER = 'XXX'
 MAIL_REVIEW_SMTP_PORT = 25
 MAIL_REVIEW_FROM_ADDR = 'DBA'
 MAIL_REVIEW_FROM_PASSWORD = ''
-MAIL_REVIEW_DBA_ADDR = ['xx@xx',]
+MAIL_REVIEW_DBA_ADDR = ['XXX',]
 
 #slave status default value
 SLAVE_IO_RUNNING = None
@@ -44,7 +44,7 @@ SLAVE_SQL_RUNNING = None
 
 #send message interface @chengyi.wu
 def send_message(telphone,message):
-    url = "xx"%(telphone,message)
+    url = "http://XXX"%(telphone,message)
     res = requests.get(url,{}).text
     if res == 'True':
         print('发送短信成功')
@@ -53,7 +53,7 @@ def send_message(telphone,message):
 
 #send mail alarm
 def send_mail(to,sub,content):
-    me = MAIL_REVIEW_FROM_ADDR+"<"+MAIL_REVIEW_FROM_ADDR+"@"+MAIL_REVIEEW_SMTP_SERVER+">"
+    me = MAIL_REVIEW_FROM_ADDR+"<"+MAIL_REVIEW_FROM_ADDR+"@"+MAIL_REVIEW_SMTP_SERVER+">"
     msg = MIMEText(content)
     msg['Subject'] = sub
     msg['From'] = me
@@ -74,20 +74,20 @@ def monitor_MySQL_replication():
     status = True
     try:
         conn = MySQLdb.connect(**MYSQL_SETTINGS)
-        cur = con.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+        cur = conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
         cur.execute("SHOW SLAVE STATUS;")
         res = cur.fetchone()
-        SLAVE_IO_RUNNING=req['Slave_IO_Running']
-        SLAVE_SQL_RUNNING=req['Slave_SQL_Running']
+        SLAVE_IO_RUNNING=res['Slave_IO_Running']
+        SLAVE_SQL_RUNNING=res['Slave_SQL_Running']
         if res['Slave_IO_Running'] == 'Yes' and res['Slave_SQL_Running'] == 'Yes':
             logging.info("OK!")
         else:
-            logging.err("Wrong!")
+            logging.error("Wrong!")
             status = False
         cur.close()
         conn.close()
     except Exception as e:
-            logging.err(e)
+            logging.error(e)
             status = False
     return status
 
@@ -107,4 +107,4 @@ if __name__ == "__main__":
     clear_log()
     if monitor_MySQL_replication():
         send_message("telephone", "Replication Error: Port_%s,Host_%s"%(MYSQL_SETTINGS['port'],MYSQL_SETTINGS['host']))
-        send_mail([email, ], "MySQL Replication Alarm", "Slave_SQL_Running：%s Slave_IO_Running: %s "%(SLAVE_SQL_RUNNING,SLAVE_IO_RUNNING))
+        send_mail("MAIL_TO", "MySQL Replication Alarm", "Slave_SQL_Running: %s Slave_IO_Running: %s "%(SLAVE_SQL_RUNNING,SLAVE_IO_RUNNING))
