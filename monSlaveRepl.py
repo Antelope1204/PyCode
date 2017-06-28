2baacb0aa75d650135d9cd9c4c2b51ea9240e0ef
Antelop1204 <lilinyang06@163.com>
2017-06-28 11:16:22 +0800

    add message

--- monSlaveRepl.py
+++ monSlaveRepl.py
@@ -5,27 +5,70 @@
 
 import os
 import MySQLdb
+import requests
 import sys
 import os.path
 import urllib
 import urllib2
 import logging
+import smtplib
+from email.mime.text import MIMEText
 
 #mysql configure
 MYSQL_SETTINGS = {
-    "host": "127.0.0.1",
-    "port": 3306,
-    "user": "cdcsync",
-    "passwd": "123456"
+    "host": "10.72.38.13",
+    "port": 6057,
+    "user": "dba",
+    "passwd": "84ndsjknma!"
 }
 #log configure
 logging.basicConfig(
-    filename='/data/tmp/mysql_repliaction_monitor.log',
-    filemode='a',
-    format='%(asctime)s %(filename)s[line:%(lineno)d] %(funcName)s %(levelname)s %(message)s',
-    datefmt='%a, %d %b %Y %H:%M:%S',
-    level=logging.INFO
+    filename = '/data/tmp/mysql_repliaction_monitor.log',
+    filemode = 'a',
+    format = '%(asctime)s %(filename)s[line:%(lineno)d] %(funcName)s %(levelname)s %(message)s',
+    datefmt = '%a, %d %b %Y %H:%M:%S',
+    level = logging.INFO
 )
+#smtp configure
+
+MAIL_REVIEW_SMTP_SERVER = 'mta1.corp.ebj.elong.com'
+MAIL_REVIEW_SMTP_PORT = 25
+MAIL_REVIEW_FROM_ADDR = 'DBA'
+MAIL_REVIEW_FROM_PASSWORD = ''
+MAIL_REVIEW_DBA_ADDR = ['linyang.li@corp.elong.com',]
+
+#slave status default value
+SLAVE_IO_RUNNING = None
+SLAVE_SQL_RUNNING = None
+
+
+#send message interface @chengyi.wu
+def send_message(telphone,message):
+    url = "http://smsweb.vip.elong.com/Default.aspx?orderno=1&phone=%s&&content=%s&bstype=alarm-005&SmsFormat=1"%(telphone,message)
+    res = requests.get(url,{}).text
+    if res == 'True':
+        print('发送短信成功')
+    else:
+        print('发送短信失败')
+
+#send mail alarm
+def send_mail(to,sub,content):
+    me = MAIL_REVIEW_FROM_ADDR+"<"+MAIL_REVIEW_FROM_ADDR+"@"+MAIL_REVIEEW_SMTP_SERVER+">"
+    msg = MIMEText(content)
+    msg['Subject'] = sub
+    msg['From'] = me
+    msg['To'] = to
+    msg['Cc'] = ';'.join(MAIL_REVIEW_DBA_ADDR)
+    try:
+        s = smtplib.SMTP()
+        s.connect(MAIL_REVIEW_SMTP_SERVER)
+        s.sendmail(me, to, msg.as_string())
+        s.close()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
 def monitor_MySQL_replication():
     #monitor mysql slave status
     status = True
@@ -34,6 +77,8 @@ def monitor_MySQL_replication():
         cur = con.cursor(cursorclass=MySQLdb.cursors.DictCursor)
         cur.execute("SHOW SLAVE STATUS;")
         res = cur.fetchone()
+        SLAVE_IO_RUNNING=req['Slave_IO_Running']
+        SLAVE_SQL_RUNNING=req['Slave_SQL_Running']
         if res['Slave_IO_Running'] == 'Yes' and res['Slave_SQL_Running'] == 'Yes':
             logging.info("OK!")
         else:
@@ -50,23 +95,9 @@ def get_hostname():
     #get hostname of Linux
     return os.environ['HOSTNAME']
 
-def send_mail(receiver,content,title):
-    # send alarm to somebody
-    rtx_url = 'http://www.example.com:11770/sendRtxByPost'
-    data = {
-        "appId"     : 6,
-        "appKey"    : 'password',
-        "username"  : receiver,
-        "title"     : title,
-        "content"   : content
-    }
-    postdata = urllib.urlencode(data)
-    req = urllib2.Request(rtx_url,postdata)
-    return urllib2.urlopen(req)
-
 def clear_log():
     #clear monitor log
-    log_file='/data/tmp/mysql_repliaction_monitor.log'
+    log_file = '/data/tmp/mysql_repliaction_monitor.log'
     size = os.path.getsize(log_file)/(1024*1024)
     if size >= 100:
         os.remove(log_file)
@@ -74,6 +105,6 @@ def clear_log():
 
 if __name__ == "__main__":
     clear_log()
-    mail_receiver = "happy;"
     if monitor_MySQL_replication():
-        send_mail(receiver=mail_receiver,content='slave replication error',title=get_hostname())
+        send_message("18510340022", "Replication Error: Port_%s,Host_%s"%(MYSQL_SETTINGS['port'],MYSQL_SETTINGS['host']))
+        send_mail([email, ], "MySQL Replication Alarm", "Slave_SQL_Running：%s Slave_IO_Running: %s "%(SLAVE_SQL_RUNNING,SLAVE_IO_RUNNING))
